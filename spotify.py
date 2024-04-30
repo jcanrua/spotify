3054c4631179dbbd2685c813a0aac937c992dd59
Jorge Canut Ruano <145457305+jcanrua@users.noreply.github.com>
2024-04-30 22:33:53 +0200

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -3,6 +3,8 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from youtubesearchpython import VideosSearch
 
+########## Spotify API authentification ##########
+
 clientID = "a7bf021e80114e5ebecf982370aaf17e"
 clientSecret = "2e4a96c283394ee7b2d4e50aaf26316a"
 redirectUrl = "https://localhost:8888/callback"
@@ -13,29 +15,12 @@ sp_auth = SpotifyOAuth(
     client_secret=clientSecret,
     redirect_uri=redirectUrl,
     scope = scope,
-    show_dialog=True
 )
 
 sp = Spotify(auth_manager=sp_auth)
 
-resultados = sp.playlist_tracks(
-    "https://open.spotify.com/playlist/3jH3pkKUTAR4XCUMks2l4z",
-    None, 100, 0, None)
 
-raw_data = resultados['items']
-while resultados['next']:
-    resultados = sp.next(resultados)
-    raw_data.extend(resultados['items'])
-
-listaFinal = []
-urls = []
-for cancion in raw_data:
-    s = VideosSearch(cancion['track']['name'] + ", " + cancion['track']['artists'][0]['name'], limit=1)
-    link = s.result()['result'][0]['link']
-    listaFinal.append(cancion['track']['name'] + " - " + cancion['track']['artists'][0]['name'])
-    urls.append(link)
-    #print(cancion['track']['name'] + ", " + cancion['track']['artists'][0]['name'] + " - " + link)
-
+############   Youtube_dl parameters  ############
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -47,5 +32,38 @@ ydl_opts = {
     'outtmpl': f'%(title)s.%(ext)s',
 }
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(urls)
+
+#################### Variables ####################
+
+raw_data = 0
+listaFinal = []
+urls = []
+playlist_url = 0
+porcentaje = 0
+
+#################### Funciones ####################
+
+def get_playlist():
+    global raw_data, listaFinal, urls
+
+    resultados = sp.playlist_tracks(playlist_url, None, 100, 0, None)
+    raw_data = resultados['items']
+
+    while resultados['next']:
+        resultados = sp.next(resultados)
+        raw_data.extend(resultados['items'])
+
+    for cancion in raw_data:
+        s = VideosSearch(cancion['track']['name'] + ", " + cancion['track']['artists'][0]['name'], limit=1)
+        link = s.result()['result'][0]['link']
+        urls.append(link)
+        listaFinal.append(cancion['track']['name'] + " - " + cancion['track']['artists'][0]['name'])
+        
+        #print(cancion['track']['name'] + ", " + cancion['track']['artists'][0]['name'] + " - " + link)
+
+
+def download():
+    global urls, ydl_opts
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(urls)
+        porcentaje = porcentaje + (len(urls)/100)
